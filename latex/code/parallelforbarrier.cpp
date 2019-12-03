154f410b5c2a9a0dcfce4b529dfe69279bdab7fc
Ronan Keryell <ronan.keryell@xilinx.com>
2019-12-03 10:11:11 -0800

    Add Apache 2.0 license to all C++ headers and examples
    This was done by applying the next script
    with latex/code/*.cpp latex/headers/*.h :
    
    function add_license() {
        file_name="$1"
        echo "// Copyright (c) 2012-2020 The Khronos Group Inc.
    //
    // Licensed under the Apache License, Version 2.0 (the "License");
    // you may not use this file except in compliance with the License.
    // You may obtain a copy of the License at
    //
    //     http://www.apache.org/licenses/LICENSE-2.0
    //
    // Unless required by applicable law or agreed to in writing, software
    // distributed under the License is distributed on an "AS IS" BASIS,
    // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    // See the License for the specific language governing permissions and
    // limitations under the License.
    " | cat - "$file_name" | sponge "$file_name"
    }
    
    for f in "$@"; do
        add_license "$f"
    done

--- latex/code/parallelforbarrier.cpp
+++ latex/code/parallelforbarrier.cpp
@@ -1,3 +1,17 @@
+// Copyright (c) 2012-2020 The Khronos Group Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the License);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an AS IS BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 myQueue.submit([&](handler & cgh) {
   cgh.parallel_for<class example_kernel>(
       nd_range<3>(range<3>(4, 4, 4), range<3>(2, 2, 2)), [=](nd_item<3> item) {
