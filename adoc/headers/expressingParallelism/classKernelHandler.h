6a18b8d0e7e0b04c27bd5ecf906c4a284d3bc6b1
Greg Lueck <gregory.m.lueck@intel.com>
2021-03-08 09:05:05 -0500

    Minor fixes to spec constant API
    * The declaration of `specialization_id` defines a class type named
      `value_type` for the underlying type of the specialization constant.
      However, all the declarations for `get_specialization_constant()` and
      `set_specialization_constant()` referred to the type name as just
      `type`.  We need to use the same name in both places.  This commit
      uses the name `value_type` consistently, but I'm happy to use `type`
      instead of we prefer.
    
    * During a previous update to the specialization constant API I removed
      the method `kernel_handler::has_specialization_constant()` because I
      did not think it could be implemented in a useful way.  However, I
      forgot to remove the declaration from the synopsis.  Do that now.
    
    * The synopsis for the `handler` class was missing the declaration of
      `set_specialization_constant()` even though we described it later in
      the specification.  Add this missing declaration.
    
    * Some of the member function declaration for specialization constants
      used the template parameter name `SpecName` while others used the
      template parameter name `S`.  Consistently use the name `SpecName` to
      make it clear that all these template parameters mean the same thing.

--- adoc/headers/expressingParallelism/classKernelHandler.h
+++ adoc/headers/expressingParallelism/classKernelHandler.h
@@ -5,8 +5,9 @@ namespace sycl {
 
 class kernel_handler {
  public:
-  template<auto& S>
-  typename std::remove_reference_t<decltype(S)>::type get_specialization_constant();
+  template<auto& SpecName>
+  typename std::remove_reference_t<decltype(SpecName)>::value_type
+  get_specialization_constant();
 };
 
 }  // namespace sycl
