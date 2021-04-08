95154eb0cbcd03171e9adcd2601f491cb8f45864
Greg Lueck <gregory.m.lueck@intel.com>
2021-04-08 08:32:52 -0400

    Add more specialization_id examples
    * Add more examples of legal and illegal declarations of
      `specialization_id` variables.
    
    * Simplify wording about declaring a `specialization_id` variable in
      class scope.  Normal C++ rules forbid a `constexpr` variable from
      being declared in a class that is inside a function.  Therefore, we
      don't need to list this as a restriction.  Instead, it's sufficient
      to just say the `specialization_id` variables may be declared in
      class scope.

--- adoc/code/specialization_id.cpp
+++ adoc/code/specialization_id.cpp
@@ -7,29 +7,38 @@ using namespace sycl; // (optional) avoids need for "sycl::" before SYCL names
 struct Mixed {int i; float f;};
 
 constexpr specialization_id<int> a{1};            // OK
-static constexpr specialization_id<int> b{2};     // OK
-specialization_id<int> c{3};                      // ERROR: not constexpr
+constexpr specialization_id<Mixed> b{2, 3.14};    // OK
+inline constexpr specialization_id<int> c{3};     // OK
+static constexpr specialization_id<int> d{4};     // OK
+specialization_id<int> e{5};                      // ILLEGAL: not constexpr
 
 namespace foo {
-  constexpr specialization_id<Mixed> c{1, 3.14};  // OK
+  constexpr specialization_id<int> same_name{6};  // OK
+}
+namespace bar {
+  constexpr specialization_id<int> same_name{7};  // OK
 }
 namespace {
-  constexpr specialization_id<Mixed> d{2, 2.718}; // OK
+  constexpr specialization_id<int> other_name{8}; // OK
+}
+namespace {
+  constexpr specialization_id<int> same_name{9};  // ILLEGAL: ambiguous name in anonymous
+                                                  // namespace
 }
 
 struct Bar {
-  static constexpr specialization_id<int> e{4};   // OK
+  static constexpr specialization_id<int> f{10};  // OK
 };
 struct Baz {
   struct Inner {
-    static constexpr specialization_id<int> f{5}; // OK
+    static constexpr specialization_id<int> g{11};// OK
   };
 };
 class Boo {
-  static constexpr specialization_id<int> g{6};   // ERROR: not public member
+  static constexpr specialization_id<int> h{12};  // ILLEGAL: not public member
 };
 
 void Func() {
-  static constexpr specialization_id<int> h{7};   // ERROR: not at namespace or class scope
+  static constexpr specialization_id<int> i{13};  // ILLEGAL: not at namespace or class scope
   /* ... */
 }
