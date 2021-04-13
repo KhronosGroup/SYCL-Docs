c91b6c08ccf64ce04e5067f5ab3c30599c94e030
Greg Lueck <gregory.m.lueck@intel.com>
2021-04-13 18:22:07 -0400

    Standard attribute position for non-lambda kernels
    We used to define the position of C++ attributes attached to kernel
    functions such that they appertained to the "function-type" of the
    kernel function.  This is necessary for lambda functions because the
    C++17 standard does not define an attribute position that appertains to
    the lambda function itself.  (This is expected to be fixed in C++23.)
    
    However, non-lambda functions have a well-defined position for
    attributes that appertain to the function itself.  It's not clear why
    the spec did not use this position for these functions.
    
    This commit changes the position of these attributes to the standard
    place.
    
    I also made some clarifications about the general restrictions of
    these attributes, which were suggested by our front-end experts.  I
    think these do not change the intent, but just tighten the wording.

--- adoc/code/attributes.cpp
+++ adoc/code/attributes.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-2021 The Khronos Group, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-// Kernel defined as a lambda
+// Kernel defined as a lambda.
 myQueue.submit([&](handler &h) {
  h.parallel_for( range<1>(16),
       [=] (item<1> it) [[sycl::reqd_work_group_size(16)]] {
@@ -9,11 +9,19 @@ myQueue.submit([&](handler &h) {
       });
 });
 
-// Kernel defined as a functor to be invoked later
-class KernelFunctor {
+// Kernel defined as a named function object.
+class KernelFunctor1 {
   public:
-  void operator()(item<1> it) const [[sycl::reqd_work_group_size(16)]] {
+  [[sycl::reqd_work_group_size(16)]]
+  void operator() (item<1> it) const {
     //[kernel code]
   };
 };
 
+// Kernel defined as a named function object.
+class KernelFunctor2 {
+  public:
+  void operator() [[sycl::reqd_work_group_size(16)]] (item<1> it) const {
+    //[kernel code]
+  };
+};
