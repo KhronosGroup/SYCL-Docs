9c7fa9d5367796d7df3861baa43184c8fde737ce
Greg Lueck <gregory.m.lueck@intel.com>
2024-06-09 11:09:02 -0400

    Merge pull request #559 from gmlueck/gmlueck/synopsis
    Add HTML links for "synopsis" code blocks

---
+++ adoc/config/synopsis.rb
@@ -0,0 +1,9 @@
+# Copyright (c) 2011-2024 The Khronos Group, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+#require 'asciidoctor/extensions' unless RUBY_ENGINE == 'opal'
+RUBY_ENGINE == 'opal' ? (require 'synopsis/extension') : (require_relative 'synopsis/extension')
+
+Asciidoctor::Extensions.register do
+    postprocessor AddSynopsisAnchors
+end
