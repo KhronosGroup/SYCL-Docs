8878588faefefdef4c90f684c0dd0b297ea401aa
Greg Lueck <gregory.m.lueck@intel.com>
2025-04-04 13:52:40 -0400

    Second step of updating the copyright dates
    Update the copyright dates by running
    "adoc/scripts/update-copyright.sh".

--- adoc/headers/algorithms/reduce.h
+++ adoc/headers/algorithms/reduce.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011-2024 The Khronos Group, Inc.
+// Copyright (c) 2011-2025 The Khronos Group, Inc.
 // SPDX-License-Identifier: MIT
 
 template <typename Group, typename Ptr, typename BinaryOperation>
