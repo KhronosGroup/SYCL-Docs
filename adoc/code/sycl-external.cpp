19fe6531fd556e21c7f71b1de84dbfcf79e27bde
Greg Lueck <gregory.m.lueck@intel.com>
2026-02-13 12:33:03 -0500

    Improve code format

--- adoc/code/sycl-external.cpp
+++ adoc/code/sycl-external.cpp
@@ -6,18 +6,14 @@
 SYCL_EXTERNAL void A1();  // OK
 void A1() { /* ... */ }   // OK; redeclaration
 void A2();                // OK
-SYCL_EXTERNAL void A2() { /* ... */
-}  // ERROR: SYCL_EXTERNAL not on first declaration.
+SYCL_EXTERNAL void A2() { /* ... */ }  // ERROR: SYCL_EXTERNAL not on first declaration.
 SYCL_EXTERNAL void A3() { /* ... */ }  // OK
 
 SYCL_EXTERNAL extern void B1();  // OK
-extern SYCL_EXTERNAL void
-B2();  // ERROR: SYCL_EXTERNAL after declaration specifiers.
+extern SYCL_EXTERNAL void B2();  // ERROR: SYCL_EXTERNAL after declaration specifiers.
 
 [[maybe_unused]] SYCL_EXTERNAL void C1();  // OK
 SYCL_EXTERNAL [[maybe_unused]] void C2();  // OK
 
-template <typename T>
-SYCL_EXTERNAL void D();  // OK
-template <>
-SYCL_EXTERNAL inline void D<int>();  // OK
+template <typename T> SYCL_EXTERNAL void D();    // OK
+template <> SYCL_EXTERNAL inline void D<int>();  // OK
