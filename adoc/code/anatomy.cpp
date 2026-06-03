121816e19c9c7601e93987c0a7b99b6d51e4d372
Michael Lance <mblance@proton.me>
2026-06-03 11:56:14 -0500

    update ananotomy.cpp to use USM

--- adoc/code/anatomy.cpp
+++ adoc/code/anatomy.cpp
@@ -1,39 +1,26 @@
-// Copyright (c) 2011-2026 The Khronos Group, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
 #include <iostream>
 #include <sycl/sycl.hpp>
-using namespace sycl;  // (optional) avoids need for "sycl::" before SYCL names
+using namespace sycl; // (optional) avoids need for "sycl::" before SYCL names
 
 int main() {
-  int data[1024];  // Allocate data to be worked on
-
   // Create a default queue to enqueue work to the default device
-  queue myQueue;
+  queue myQueue{};
+  // Allocate shared memory to be worked on on the default device
+  int *A_shared = malloc_shared<int>(1024, myQueue);
 
-  // By wrapping all the SYCL work in a {} block, we ensure
-  // all SYCL tasks must complete before exiting the block,
-  // because the destructor of resultBuf will wait
-  {
-    // Wrap our data variable in a buffer
-    buffer resultBuf{data, {1024}};
+  // Enqueue a parallel_for task with 1024 work-items
+  myQueue.parallel_for(1024, [=](id<1> idx) {
+    // Initialize each buffer element with its own rank number starting at 0
+    A_shared[idx] = idx;
+  }); // End of the kernel function
 
-    // Create a command group to issue commands to the queue
-    myQueue.submit([&](handler& cgh) {
-      // Request write access to the buffer without initialization
-      accessor writeResult{resultBuf, cgh, write_only, no_init};
+  myQueue.wait(); // Wait for the queue to finish executing on the device
 
-      // Enqueue a parallel_for task with 1024 work-items
-      cgh.parallel_for(1024, [=](id<1> idx) {
-        // Initialize each buffer element with its own rank number starting at 0
-        writeResult[idx] = idx;
-      });  // End of the kernel function
-    });    // End of our commands for this queue
-  }  // End of scope, so we wait for work producing resultBuf to complete
+  // Reclaim memory on the host and the device
+  free(A_shared, myQueue);
 
   // Print result
   for (int i = 0; i < 1024; i++)
-    std::cout << "data[" << i << "] = " << data[i] << std::endl;
-
+    std::cout << "A_shared[" << i << "] = " << A_shared[i] << std::endl;
   return 0;
 }
