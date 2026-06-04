33012a725ad084a80e3c2dd0f814a2ab5063aaae
Michael Lance <mblance@proton.me>
2026-06-04 15:25:53 -0500

    Use in_order queue and malloc_device

--- adoc/code/anatomy.cpp
+++ adoc/code/anatomy.cpp
@@ -2,25 +2,32 @@
 #include <sycl/sycl.hpp>
 using namespace sycl; // (optional) avoids need for "sycl::" before SYCL names
 
+std::vector<int> A_host(1024);
+
 int main() {
   // Create a default queue to enqueue work to the default device
-  queue myQueue{};
+  queue myQueue{property::queue::in_order()};
+
   // Allocate shared memory to be worked on on the default device
-  int *A_shared = malloc_shared<int>(1024, myQueue);
+  int *A_device = malloc_device<int>(1024, myQueue);
 
   // Enqueue a parallel_for task with 1024 work-items
   myQueue.parallel_for(1024, [=](id<1> idx) {
     // Initialize each buffer element with its own rank number starting at 0
-    A_shared[idx] = idx;
+    A_device[idx] = idx;
   }); // End of the kernel function
+
+  // Copy the results back to the device from the host
+  myQueue.copy(A_device, A_host.data(), 1024);
 
   myQueue.wait(); // Wait for the queue to finish executing on the device
 
-  // Reclaim memory on the host and the device
-  free(A_shared, myQueue);
-
   // Print result
   for (int i = 0; i < 1024; i++)
-    std::cout << "A_shared[" << i << "] = " << A_shared[i] << std::endl;
+    std::cout << "A_shared[" << i << "] = " << A_host[i] << std::endl;
+
+  // Reclaim memory on the host and the device
+  free(A_device, myQueue);
+
   return 0;
 }
