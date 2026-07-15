47feb8a32bba6878ae903eaa33574f8827022ff3
Michael Lance <mlance4@uwyo.edu>
2026-07-15 10:13:54 -0500

    define 1024 as a constant expr

--- adoc/code/anatomy.cpp
+++ adoc/code/anatomy.cpp
@@ -7,28 +7,31 @@
 using namespace sycl; // (optional) avoids need for "sycl::" before SYCL names
 
 int main() {
+  // Declare number of work items
+  constexpr size_t N = 1024;
+
   // Allocate host memory to store the results
-  std::vector<int> dataHost(1024);
+  std::vector<int> dataHost(N);
 
   // Create an in order queue to enqueue work to the default device
   queue myQueue{property::queue::in_order()};
 
   // Allocate device memory to be worked on
-  int *dataDevice = malloc_device<int>(1024, myQueue);
+  int *dataDevice = malloc_device<int>(N, myQueue);
 
   // Enqueue a parallel_for task with 1024 work-items
-  myQueue.parallel_for(1024, [=](id<1> idx) {
+  myQueue.parallel_for(N, [=](id<1> idx) {
     // Initialize each buffer element with its own rank number starting at 0
     dataDevice[idx] = idx;
   }); // End of the kernel function
 
   // Copy the results back to the host from the device
-  myQueue.copy(dataDevice, dataHost.data(), 1024);
+  myQueue.copy(dataDevice, dataHost.data(), N);
 
   myQueue.wait(); // Wait for the queue to finish executing all the tasks
 
   // Print result
-  for (int i = 0; i < 1024; i++)
+  for (int i = 0; i < N; i++)
     std::cout << "dataHost[" << i << "] = " << dataHost[i] << std::endl;
 
   // Free device memory
